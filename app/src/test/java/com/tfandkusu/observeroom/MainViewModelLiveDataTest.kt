384eab31df22a5ac308773b83db35d9141587969
Haruhiko Takada <haruhiko.takada@gmail.com>
2020-11-28 23:15:37 +0900

    LiveDataを返却するメソッドを使用するクラスの単体テストを作成する。

--- app/src/test/java/com/tfandkusu/observeroom/MainViewModelLiveDataTest.kt
+++ app/src/test/java/com/tfandkusu/observeroom/MainViewModelLiveDataTest.kt
@@ -6,6 +6,7 @@ import com.tfandkusu.observeroom.datastore.Division
 import com.tfandkusu.observeroom.datastore.Member
 import com.tfandkusu.observeroom.datastore.MemberLocalDataStore
 import com.tfandkusu.observeroom.datastore.MemberWithDivision
+import com.tfandkusu.observeroom.util.LiveDataTestUtil
 import com.tfandkusu.observeroom.view.main.MainViewModelLiveData
 import io.kotlintest.shouldBe
 import io.mockk.MockKAnnotations
@@ -32,9 +33,6 @@ class MainViewModelLiveDataTest {
 
     @Test
     fun test() {
-        // このテストはうまくいかなかった
-        if (true)
-            return
         every {
             localDataStore.listMembersLiveData()
         } returns MutableLiveData<List<MemberWithDivision>>(
@@ -51,15 +49,17 @@ class MainViewModelLiveDataTest {
         )
         // テスト対象を作る
         val viewModel = MainViewModelLiveData(localDataStore)
+        val items = LiveDataTestUtil.getValue(viewModel.items)
+        val progress = LiveDataTestUtil.getValue(viewModel.progress)
         // リストが更新された
         // valueがnullになる
-        viewModel.items.value?.get(0)?.id shouldBe 1L
-        viewModel.items.value?.get(0)?.memberName shouldBe "name1"
-        viewModel.items.value?.get(0)?.divisionName shouldBe "Sales"
-        viewModel.items.value?.get(1)?.id shouldBe 3L
-        viewModel.items.value?.get(1)?.memberName shouldBe "name2"
-        viewModel.items.value?.get(1)?.divisionName shouldBe "Development"
+        items[0].id shouldBe 1L
+        items[0].memberName shouldBe "name1"
+        items[0].divisionName shouldBe "Sales"
+        items[1].id shouldBe 3L
+        items[1].memberName shouldBe "name2"
+        items[1].divisionName shouldBe "Development"
         // プログレスが消えた
-        viewModel.progress.value shouldBe false
+        progress shouldBe false
     }
 }
